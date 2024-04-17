9dcb0cd7ea0634429759a4e4e70003ee1c8de7fe
heee1 <p1xell@kakao.com>
2024-04-17 16:43:42 +0900

    [기업과제] Github RestAPI 데이터 요청,응답 연결완료
    - 완료 사항
        - Github RestAPI + Axios를 통해 issues데이터를 받아도록 구현하였습니다.
        - 5번째 순서마다 광고가 출력되도록 구현하였습니다. ( 고양이 이미지 )
    - 진행중인 사항
        - 한 페이지에 데이터를 10개, 총 10페이지를 페이로 구분하여
          보여줄 수 있도록 무한스크롤기능을 도입할 예정입니다.
        - Card를 클릭하면 Detail페이지로 이동할 수 있도록 라우팅 작업을
          진행할 예정입니다.

--- WEEK3/Day2_assignment/corporate_task_ex/src/App.jsx
+++ WEEK3/Day2_assignment/corporate_task_ex/src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
-import styled from 'styled-components';
-import Header from './components/common/Header';
-import router from './router/router';
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./components/Header";
+import router from "./router/router";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Header />
-                {router}
-            </Layout>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Header />
+        {router}
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
 
 const Layout = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
 `;
--- WEEK3/Day2_assignment/corporate_task_ex/src/pages/List.jsx
+++ WEEK3/Day2_assignment/corporate_task_ex/src/pages/List.jsx
@@ -1,68 +1,110 @@
-import styled from 'styled-components';
-import dummydatas from '../data/dummyData.json';
-import ADimg from '../assets/IMG_2126.jpg';
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import ADimg from "../assets/IMG_2126.jpg";
 
 const List = () => {
-    return (
-        <CardListArea>
-            {dummydatas.map((data) => {
-                if (data.id === 'AD') {
-                    return (
-                        <Card key={data.id}>
-                            <ImgContainer href="https://www.wanted.co.kr/">
-                                <Img src={ADimg} alt="ADimg" />
-                            </ImgContainer>
-                        </Card>
-                    );
-                } else {
-                    return (
-                        <Card key={data.id}>
-                            <CardHeader>
-                                {data.id} {data.title}
-                            </CardHeader>
-                            <CardContent>
-                                {data.author} {data.date}
-                            </CardContent>
-                            <CardComment>코멘트 : {data.comments}</CardComment>
-                        </Card>
-                    );
-                }
-            })}
-        </CardListArea>
-    );
+  const [issues, setIssues] = useState([]);
+  const [page, setPage] = useState(1); // 페이지 상태관리
+  const perPage = 10; // 페이지당 보여줄 데이터 개수
+
+  useEffect(() => {
+    const requestURL = `https://api.github.com/repos/angular/angular-cli/issues`;
+
+    const requestIssues = async () => {
+      try {
+        const response = await Axios.get(requestURL, {
+          params: {
+            state: "open",
+            sort: "comments",
+            direction: "desc",
+            per_page: perPage,
+            page: page,
+          },
+        });
+        console.log(response);
+        setIssues((prevIssues) => [...prevIssues, ...response.data]);
+      } catch (error) {
+        console.error("Request Error >> ", error);
+      }
+    };
+    requestIssues();
+  }, [page]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <CardListArea>
+      {issues.map((issue, index) => {
+        if ((index + 1) % 5 === 0) {
+          // 5번째 마다 광고 출력
+          return (
+            <Card key={`AD-${index}`}>
+              <ImgContainer href='https://www.wanted.co.kr/'>
+                <Img src={ADimg} alt='ADimg' />
+              </ImgContainer>
+            </Card>
+          );
+        } else {
+          // 나머지는 이슈 출력
+          return (
+            <Card key={issue.id}>
+              <CardHeader>
+                {issue.number} {issue.title}
+              </CardHeader>
+              <CardContent>
+                {issue.user.login} {issue.created_at}
+              </CardContent>
+              <CardComment>코멘트 : {issue.comments}</CardComment>
+            </Card>
+          );
+        }
+      })}
+    </CardListArea>
+  );
 };
 
 export default List;
 
 const CardListArea = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 500px;
-    height: 500px;
-    gap: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 500px;
+  gap: 10px;
 `;
 
 const Card = styled.div`
-    position: relative;
-    width: 400px;
-    height: 300px;
-    padding: 5px;
-    border-bottom: 2px black solid;
+  position: relative;
+  width: 400px;
+  padding: 5px;
+  border-bottom: 2px black solid;
 `;
 
 const CardHeader = styled.div``;
 const CardContent = styled.div``;
 const CardComment = styled.div`
-    position: absolute;
-    right: 5px;
-    top: 20px;
+  position: absolute;
+  right: 5px;
+  top: 20px;
 `;
 const ImgContainer = styled.a`
-    width: inherit;
-    height: inherit;
+  width: inherit;
+  height: inherit;
 `;
 const Img = styled.img`
-    width: inherit;
-    height: inherit;
+  width: inherit;
+  height: inherit;
 `;
